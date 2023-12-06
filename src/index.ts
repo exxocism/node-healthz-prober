814b478a5270604e89e6e33d7e63ae3cb1db04ab
Minwoo <minwoo.choi@unity3d.com>
2023-12-06 20:19:19 +0900

    feat: implement main function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello, world!');
+import { getConfig } from './lib/getConfig';
+import { healthCheck } from './lib/healthCheck';
+
+const mainFunction = () => {
+  let index = 0;
+  while (true) {
+    index++;
+    const { url, webhook } = getConfig(index);
+    if (!url) break;
+
+    console.log(`[${index}] Checking ${url}...`);
+    healthCheck(url, webhook);
+  }
+};
+
+mainFunction();
