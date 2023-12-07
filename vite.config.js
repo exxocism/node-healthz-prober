66586884f7badacaf00ecd19c13ce4f6b14f8873
Minwoo <minwoo.choi@unity3d.com>
2023-12-07 20:32:23 +0900

    feat: add vite config file

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig, loadEnv } from 'vite';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default ({ mode }) => {
+  return defineConfig({
+    build: {
+      rollupOptions: {
+        plugins: [commonjs()],
+        // overwrite default .html entry
+        input: 'dist/index.js',
+        output: {
+          manualChunks: false,
+          inlineDynamicImports: true,
+          entryFileNames: '[name].js',
+          assetFileNames: '[name].[ext]',
+        },
+      },
+    },
+    define: {
+      'process.env': { ...loadEnv(mode, process.cwd()) },
+    },
+  });
+};
